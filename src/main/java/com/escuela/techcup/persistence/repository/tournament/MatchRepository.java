281aa821cb1d5da754a294a69b1277af6853850d
CamiloWGod4564 <juan.cristancho-v@mail.escuelaing.edu.co>
2026-04-06 11:17:32 -0500

    metodo de busqueda en MatchRepository

--- src/main/java/com/escuela/techcup/persistence/repository/tournament/MatchRepository.java
+++ src/main/java/com/escuela/techcup/persistence/repository/tournament/MatchRepository.java
@@ -1,11 +1,15 @@
 package com.escuela.techcup.persistence.repository.tournament;
 
+import com.escuela.techcup.core.model.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.escuela.techcup.persistence.entity.tournament.MatchEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface MatchRepository extends JpaRepository<MatchEntity, String> {
 
@@ -14,5 +18,8 @@ public interface MatchRepository extends JpaRepository<MatchEntity, String> {
     List<MatchEntity> findByRefereeId(String refereeId);
 
     List<MatchEntity> findBySoccerFieldId(String soccerFieldId);
+
+    @Query("SELECT m FROM MatchEntity m WHERE m.id = :matchId " + "AND (m.teamA.id = :teamId OR m.teamB.id = :teamId)")
+    Optional<MatchEntity> findByIdAndTeam(@Param("matchId") String matchId, @Param("teamId") String teamId);
 
 }
